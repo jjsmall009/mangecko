5c7213672e7b2deae0a031c1f70f6d9a7ca7f895
jjsmall009 <jjsmall009@gmail.com>
2021-09-21 19:20:26 -0700

    Started work on local file reading and parsing

---
+++ src/dir_scanner.py
@@ -0,0 +1,26 @@
+# JJ Small
+# Manga Volume Tracker
+# dir_scanner.py - Code to scan a directory and get a list of manga title/volume count
+import os
+
+path_name = "D:\Manga\TEST_FILES"
+root_dir = os.scandir(path_name)
+
+# Traverse one level down for each folder in the path and count how many volumes there are
+series_volumes = {}
+file_ext = (".cbz", ".cbr", ".zip")
+for series in root_dir:
+    current_path = os.path.join(path_name, series.name)
+    current_dir = os.scandir(current_path)
+    volume_count = 0
+    for volume in current_dir:
+        if volume.is_file():
+            if volume.name.endswith(file_ext):
+                volume_count += 1
+
+    # A series has to has at least 1 valid volume
+    if volume_count > 0:
+        series_volumes[series.name] = volume_count
+
+for title, volumes in series_volumes.items():
+    print(f"{title} - {volumes} volumes")
